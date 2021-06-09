c5c65eb312c2fc764d241441f9c73d5e0d6ceb3c
denizat <deniza.telci@gmail.com>
2021-06-08 21:20:43 -0500

    Initial Commit

---
+++ texlint.py
@@ -0,0 +1,80 @@
+import sys
+
+farg = sys.argv[1]
+
+texFile = open(farg,'r')
+doc = texFile.readlines()
+
+# What I want to do:
+#     I want to make a tool that:
+#     splits up paragraps based off of sentences
+#     Indents the inside of chapters, sections, subsections, etc
+
+# This removes all leading whitespace
+def rmWs(lines):
+    out = []
+    for line in lines:
+        while line[0] == " " or line[0] =="\t":
+            line = line[1:]
+        out.append(line)
+    return out
+
+doc = rmWs(doc)
+
+# This removes all double spaces
+def rmDs(lines):
+    out = ['']
+    newline = False
+    # for i in range(len(lines)-1):
+    for line in lines:
+        if line != "\n" or out[-1] != "\n":
+            out.append(line)
+    return out[1:]
+
+doc = rmDs(doc)
+
+
+def indent(lines):
+    out = []
+    currentLevel = ''
+    commands = ["\\section","\\subsection"]
+    indentLevel = 0
+    beginStack = []
+    for line in lines:
+        # First indent
+        indt = ""
+        for i in range(indentLevel):
+            indt += "\t"
+        out.append(indt + line)
+        
+        # Then check how we should indent next line
+        for i in range(len(commands)):
+            res = line.find(commands[i])
+            # If there is an indent command
+            if res != -1:
+                currentLevel = commands[res]
+                indentLevel = i+1
+                break
+            
+        # If there is a begin command
+        if "begin" in line:
+            beginStack.append(indentLevel)
+            indentLevel += 1
+        # If there is an end command
+        if "\\end" in line:
+            indentLevel = beginStack.pop()
+            out.pop()
+            indt = ""
+            for i in range(indentLevel):
+                indt += "\t"
+            out.append(indt +line)
+    return out
+    
+doc = indent(doc)
+
+
+
+l = open("out.tex","w")
+l.writelines(doc)
+l.close()
+
